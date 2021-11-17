c97f55eb0e9aaf907dfc3b765dd02b2c6bbb2163
MARKILoff <81617746+MARKILoff@users.noreply.github.com>
2021-11-17 14:30:25 +0500

    Add files via upload

---
+++ OTASH/app.js
@@ -0,0 +1,12 @@
+let options = {
+    strings: ['Sayfuloo...', 'Eeee','Sayfuloo...','Jiyan qalesan','tincmisan mazen', 'yaxshimi🖐🖐🖐',
+    'bugn kelasamiii kottt'],
+    typeSpeed: 60, // yoziw tezligi
+    startDelay: 2000,// bowlanish tezligi
+    backSpeed: 50,// ochirish tezligi
+    backDeplay: 50,// yozb bogandan ken ociradigan
+    loop: true,
+    loopCount: Infinity,
+};
+
+let typed = new Typed('.element', options);
